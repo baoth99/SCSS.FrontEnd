72e0707b071ad3ddc10df53215f2b42f57108f7a
BaoTH <62712557+Baoth99@users.noreply.github.com>
2021-07-14 21:30:05 +0700

    sadsada

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, {createRef} from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-
 import { Provider } from 'react-redux';
-
 import "assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/scss/argon-dashboard-react.scss";
-
-
 import ProtectedRoute from './Infrastucture/routes/ProtectedRoute';
-
 import SigninOidc from './Presentation/views/redirect/SigninOidc';
 import SignoutOidc from './Presentation/views/redirect/SignoutOidc';
 
@@ -21,15 +16,10 @@ import Store from './Application/redux/stores/ApplicationStore';
 import GlobalLoading from './Presentation/components/Loadings/GlobalLoading';
 import Collapse from '@material-ui/core/Collapse';
 import { SnackbarProvider } from 'notistack';
-
 import NotifierSnack from './Presentation/components/Commons/NotifierSnack';
 import { MdCancel } from "react-icons/md";
-
-
 import { ConnectedRouter } from 'connected-react-router';
 import history from './Infrastucture/routes/History';
-
-
 
 const App = () => {
 
